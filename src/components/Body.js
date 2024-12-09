7ce628904c0582eadaa108192c411055aeb88a77
surbhi1418 <surbhilucky05@gmail.com>
2024-12-10 02:41:49 +0530

    Using Shimmer, API , useState , useFetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,65 @@
-import { useState } from "react";
-import { mockData } from "../utils/mockData";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  let [stateData, setStateData] = useState(mockData);
-  return (
+  const [stateData, setStateData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.918051364750095&lng=77.64502745121717&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    json.data.cards.forEach((element) => {
+      if (
+        element.card.card["@type"] ===
+          "type.googleapis.com/swiggy.gandalf.widgets.v2.GridWidget" &&
+        element.card.card.id === "top_brands_for_you"
+      ) {
+        setStateData(
+          element?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+        setFilterData(
+          element?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        );
+      }
+    });
+  };
+
+  const [searchText, setSearchText] = useState("");
+
+  return stateData.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
-      <div className="search-container" key="search">
-        Search
-      </div>
       <div className="fliter">
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input-container"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //Filter the restaurant
+              // search Text
+              console.log(searchText);
+              const filterData = stateData.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilterData(filterData);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="fliter-btn"
           onClick={() => {
@@ -23,8 +74,13 @@ const Body = () => {
       </div>
 
       <div className="restaurant-container">
-        {stateData.map((info) => {
-          return <RestaurantCard key={info.info.id} restaurantData={info} />;
+        {filterData.map((restaurant) => {
+          return (
+            <RestaurantCard
+              key={restaurant.info?.id}
+              restaurantData={restaurant.info}
+            />
+          );
         })}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,11 +1,10 @@
 import { CDN_URL } from "../utils/constants";
-import Body from "./Body";
 const RestaurantCard = (props) => {
   const { restaurantData } = props;
 
   const { name, cuisines, avgRating, costForTwo, cloudinaryImageId } =
-    restaurantData?.info;
-  const { deliveryTime } = restaurantData?.info?.sla;
+    restaurantData;
+  const { deliveryTime } = restaurantData?.sla;
   return (
     <div className="restaurant-card-container">
       <img
@@ -13,12 +12,13 @@ const RestaurantCard = (props) => {
         alt="restaurant-logo"
         src={CDN_URL + cloudinaryImageId}
       />
-
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} star</h4>
-      <h4>{costForTwo} </h4>
-      <h4>{deliveryTime} minutes</h4>
+      <div className="restaurant-detail-container">
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h4>{avgRating} star</h4>
+        <h4>{costForTwo} </h4>
+        <h4>{deliveryTime} minutes</h4>
+      </div>
     </div>
   );
 };
